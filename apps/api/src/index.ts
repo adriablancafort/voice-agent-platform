6b00b92344989c9b34aa16b8643dd440b2b58c31
Adrià <adriablancafort@gmail.com>
2026-05-16 18:20:46 +0200

    api cors

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,7 +1,20 @@
+import 'dotenv/config';
 import { serve } from '@hono/node-server'
 import { Hono } from 'hono'
+import { cors } from "hono/cors"
 
 const app = new Hono()
+
+const frontendUrl = process.env.FRONTEND_URL ?? "http://localhost:5173"
+const port = Number(process.env.PORT ?? "3000")
+
+app.use(
+  "*",
+  cors({
+    origin: [frontendUrl],
+    credentials: true,
+  })
+)
 
 app.get('/', (c) => {
   return c.text('Hello Hono!')
@@ -9,7 +22,7 @@ app.get('/', (c) => {
 
 serve({
   fetch: app.fetch,
-  port: 3000
+  port
 }, (info) => {
   console.log(`Server is running on http://localhost:${info.port}`)
 })
