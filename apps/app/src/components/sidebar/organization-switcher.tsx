b1fa945ff65e3aba907451b2c9c5648a3dc42bb1
Adrià <adriablancafort@gmail.com>
2026-08-15 23:55:16 +0200

    improve org selector logo

--- apps/app/src/components/sidebar/organization-switcher.tsx
+++ apps/app/src/components/sidebar/organization-switcher.tsx
@@ -89,7 +89,7 @@ function OrganizationSwitcherContent() {
               />
             }
           >
-            <div className="flex aspect-square size-8 items-center justify-center rounded-lg bg-sidebar-primary text-sidebar-primary-foreground">
+            <div className="flex aspect-square size-8 items-center justify-center rounded-lg border">
               <GalleryVerticalEndIcon className="size-4" />
             </div>
             <div className="grid flex-1 text-left text-sm leading-tight">
